bfa9495fbe56f0af07c03f1e57bd277cd925eeb0
Melanie Rogoff <mgrogoff@gmail.com>
2020-04-15 14:56:51 -0700

    Images now display

--- src/Components/Projects/Trial.js
+++ src/Components/Projects/Trial.js
@@ -4,37 +4,37 @@ import Project from "./Project.js";
 const projectApps = [
     {
         name: "Eat-Da-Burger!",
-        src: "../../images/burgerapp.png",
+        src: require ("../../images/burgerapp.png"),
         github: "https://github.com/MelanieRogoff/burger",
         deployed: "https://floating-gorge-77305.herokuapp.com/"
     },
     {
         name: "Legends of Tomorrow Employee Directory",
-        src: "../../images/lotapp.png",
+        src: require ("../../images/lotapp.png"),
         github: "https://github.com/MelanieRogoff/legendsoftomorrowapp",
         deployed: "https://lotapp.netlify.com/"
     },
     {
         name: "Weather Dashboard",
-        src: '../../images/weatherapp.png',
+        src: require('../../images/weatherapp.png'),
         github: "https://github.com/MelanieRogoff/Unit6HW",
         deployed: "https://melanierogoff.github.io/Unit6HW/"
     },
     {
         name: "Track That Track",
-        src: '../../images/trackthattrack.png',
+        src: require('../../images/trackthattrack.png'),
         github: "https://github.com/Bearpaw11/TrackThatTrack",
         deployed: "https://bearpaw11.github.io/TrackThatTrack/"
     },
     {
         name: "Code Quiz",
-        src: '../../images/quiz.png',
+        src: require('../../images/quiz.png'),
         github: "https://github.com/MelanieRogoff/Unit4HW",
         deployed: "https://melanierogoff.github.io/Unit4HW/"
     },
     {
         name: "PokeMood",
-        src: '../../images/pokemood.png',
+        src: require('../../images/pokemood.png'),
         github: "https://github.com/MelanieRogoff/PokeMood",
         deployed: "https://murmuring-mountain-45065.herokuapp.com/"
     },
