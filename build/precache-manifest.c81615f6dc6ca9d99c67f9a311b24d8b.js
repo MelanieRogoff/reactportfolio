682f49c2034fec49a4a31779e86000a00626aa8a
Melanie Rogoff <mgrogoff@gmail.com>
2020-08-11 18:29:57 -0700

    Update

---
+++ build/precache-manifest.c81615f6dc6ca9d99c67f9a311b24d8b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f6b4b496de6aca4245e2231cdfbe27c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1461e8097b2d1ccffe86",
+    "url": "/static/css/main.8d1266e4.chunk.css"
+  },
+  {
+    "revision": "8f46a9e3e33c24eae877",
+    "url": "/static/js/2.5549e81f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5549e81f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1461e8097b2d1ccffe86",
+    "url": "/static/js/main.1cefe1e5.chunk.js"
+  },
+  {
+    "revision": "586fdfc8a8030a7117d4",
+    "url": "/static/js/runtime-main.0eeac3f4.js"
+  },
+  {
+    "revision": "df9409c8238be6ba096dbf4704222e4c",
+    "url": "/static/media/bcg.df9409c8.jpeg"
+  },
+  {
+    "revision": "220af387466af5454b0680590d74da94",
+    "url": "/static/media/burgerapp.220af387.png"
+  },
+  {
+    "revision": "5c0be235ec59838a4667fe287f541a27",
+    "url": "/static/media/headshot.5c0be235.jpg"
+  },
+  {
+    "revision": "3d2f52f4ec6c335163ec7419a96cb62e",
+    "url": "/static/media/lotapp.3d2f52f4.png"
+  },
+  {
+    "revision": "9794550aca4665541af0c7c74dc39818",
+    "url": "/static/media/phone.9794550a.jpeg"
+  },
+  {
+    "revision": "61d5a57d6f5470dccc54e8e0df74f0c3",
+    "url": "/static/media/pokemood.61d5a57d.png"
+  },
+  {
+    "revision": "21144dc89a8c01374784a305ae20d8b6",
+    "url": "/static/media/quiz.21144dc8.png"
+  },
+  {
+    "revision": "3952980878e0a0a4513939b821318c23",
+    "url": "/static/media/resume.39529808.png"
+  },
+  {
+    "revision": "1cd4225db7b2787c5f60ab188009f562",
+    "url": "/static/media/trackthattrack.1cd4225d.png"
+  },
+  {
+    "revision": "3a8ec8a82bbf930af3d30ca33bd5f1da",
+    "url": "/static/media/weatherapp.3a8ec8a8.png"
+  }
+]);
--- src/Components/Pages/About.js
+++ src/Components/Pages/About.js
@@ -15,8 +15,10 @@ function About() {
                             
                     <div className="card">
                         <p className="card-text">My name is Melanie Rogoff, and I am a Full-Stack Developer. I am passionate about building user-friendly, aesthetically pleasing projects.</p>
-                        <p className="card-text">I have a Bachelor's of Science in Business Management from Arizona State University (4.0 GPA) as well as a Full-Stack Web Development Certificate from The University of Arizona (4.0 GPA).</p>
+                        <p className="card-text">I have a Bachelor's of Science in Business Management from Arizona State University (4.0 GPA) as well as a Full-Stack Web Development Certificate from The University of Arizona.</p>
                         <p className="card-text">Current Languages Include:</p>
+                       
+                            <i className="iconSizing devicon-angularjs-plain"></i>
                             <i className="iconSizing devicon-bootstrap-plain"></i><i className="iconSizing devicon-css3-plain"></i>
                             <i className="iconSizing devicon-express-original"></i><i className="iconSizing devicon-heroku-plain"></i>
                             <i className="iconSizing devicon-html5-plain"></i><i className="iconSizing devicon-javascript-plain"></i>
